6d06a67d8337d7f689bae59d9893c06e5937f31d
H. Watanabe <kaityo@users.sourceforge.jp>
2026-06-05 20:06:03 +0900

    :memo: updates

--- 05_percolation/script/zombie.py
+++ 05_percolation/script/zombie.py
@@ -10,14 +10,14 @@ weeks = 0
 r = float(sys.argv[1])
 
 for i in range(1000):
+    print(f"{weeks} {z} {z_total} {N}")
     weeks += 1
     z_total += z
+    z = int(z*r)
     N = N - z
     if N < 0:
         N = 0
-    print(f"{i+1} {z} {z_total} {N}")
-    z = int(z*r)
     if z == 0 or N == 0:
         break
 print()
-print(f"{weeks} weeks later, N={N}")
+print(f"{weeks} weeks later, Z_total = {z_total} N={N}")
