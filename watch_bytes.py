5cc9e17365e3a6f1c4e97a1a23bb0ded32192e3d
H. Watanabe <kaityo@users.sourceforge.jp>
2026-06-07 12:09:47 +0900

    :memo: updates

--- watch_bytes.py
+++ watch_bytes.py
@@ -2,19 +2,23 @@
 from pathlib import Path
 import time
 
-INTERVAL_SEC = 60
+INTERVAL_SEC = 300
 OUTFILE = "bytes.dat"
+
 
 def total_bytes() -> int:
     return sum(p.stat().st_size for p in Path(".").glob("*/*.md") if p.is_file())
+
 
 def main():
     minute = 0
     with open(OUTFILE, "w", buffering=1) as f:
         while True:
+            print(f"{minute} {total_bytes()}")
             f.write(f"{minute} {total_bytes()}\n")
-            minute += 1
+            minute += INTERVAL_SEC / 60
             time.sleep(INTERVAL_SEC)
+
 
 if __name__ == "__main__":
     main()
